e82a8052bc28fea93aa8a587672dbca4e873b7d2
shafique <shafique.md18@gmail.com>
2020-06-18 20:38:28 +0530

    userdata table is now responsive

--- resources/views/userdata.blade.php
+++ resources/views/userdata.blade.php
@@ -5,28 +5,30 @@
 <div class="card mb-4">
     <div class="card-body">
 		<h4 style="text-decoration: underline; font-family: Open Sans">User Data</h4>
-		<table class="table">
-		  <thead>
-		    <tr>
-		      <th scope="col">#</th>
-		      <th scope="col">Name</th>
-		      <th scope="col">Email</th>
-		      <th scope="col">Pincode</th>
-		    </tr>
-		  </thead>
-		  <tbody>
-		  	@isset($records)
-			    @foreach ($records as $record)
-				  	<tr>
-				      <th scope="row">{{ $loop->index + 1 }}</th>
-				      <td>{{ $record->name }}</td>
-				      <td>{{ $record->email }}</td>
-				      <td>{{ $record->pincode }}</td>
-				    </tr>
-				@endforeach
-			@endisset
-		  </tbody>
-		</table>
+		<div style="overflow-x: auto;">
+			<table class="table">
+			  <thead>
+			    <tr>
+			      <th scope="col">#</th>
+			      <th scope="col">Name</th>
+			      <th scope="col">Email</th>
+			      <th scope="col">Pincode</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			  	@isset($records)
+				    @foreach ($records as $record)
+					  	<tr>
+					      <th scope="row">{{ $loop->index + 1 }}</th>
+					      <td>{{ $record->name }}</td>
+					      <td>{{ $record->email }}</td>
+					      <td>{{ $record->pincode }}</td>
+					    </tr>
+					@endforeach
+				@endisset
+			  </tbody>
+			</table>
+		</div>
 		@empty($records)
 			<div class="{{ $message['classes'] }}">
               <i class="fas fa-times pr-2"></i>
